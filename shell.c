547339315264e6da888c110f460bb2dfd936caba
Eric Huang <officialehuang@gmail.com>
2025-10-13 17:06:47 -0700

    10/13

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include "shell.h"
 #include "msgs.h"
+#include <pwd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -15,11 +16,9 @@ void internal_handle(char **argv) {
 
     // error checks exit
     if (argv[1]) {
-      char *EXIT_ERR = "exit: ";
-      write(STDERR_FILENO, EXIT_ERR, strlen(EXIT_ERR));
+      write(STDERR_FILENO, "exit: ", 6);
       write(STDERR_FILENO, TMA_MSG, strlen(TMA_MSG));
-      char *NEWL = "\n";
-      write(STDERR_FILENO, NEWL, strlen(NEWL));
+      write(STDERR_FILENO, "\n", 1);
     }
 
     else
@@ -28,13 +27,20 @@ void internal_handle(char **argv) {
     ////////////////////////////////////////////////////////////////////////////////////////////
     // PWD HANDLER
   } else if (strcmp(argv[0], "pwd") == 0) {
+    if (argv[1] != NULL) {
+      write(STDERR_FILENO, "pwd: ", 5);
+      write(STDERR_FILENO, TMA_MSG, strlen(TMA_MSG));
+      write(STDERR_FILENO, "\n", 1);
+    }
 
     char cwd[1024];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
       write(STDOUT_FILENO, cwd, strlen(cwd));
-      write(STDOUT_FILENO, "$\n", 2);
+      write(STDOUT_FILENO, "\n", 1);
     } else {
+      write(STDERR_FILENO, "pwd: ", 5);
       write(STDERR_FILENO, GETCWD_ERROR_MSG, strlen(GETCWD_ERROR_MSG));
+      write(STDERR_FILENO, "\n", 1);
     }
     ////////////////////////////////////////////////////////////////////////////////////////////
     // CD HANDLER (FIX ME FIX ME)
@@ -43,7 +49,9 @@ void internal_handle(char **argv) {
     char cwd[1024];
 
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
+      write(STDOUT_FILENO, "cd: ", 4);
       write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+      write(STDOUT_FILENO, "\n", 1);
       return;
     }
 
@@ -56,130 +64,117 @@ void internal_handle(char **argv) {
     }
 
     const char *target = argv[1];
-    char new_dir[1024];
-    new_dir[0] = '\0';
-
-    // COPY HELPERS
-    int i = 0, j = 0;
 
     // GO HOME WITH NOTHING OR ~
     if (target == NULL || strcmp(target, "~") == 0) {
       const char *home = getenv("HOME");
-      if (home == NULL) {
+      if (!home) {
+        struct passwd *pw = getpwuid(getuid());
+        if (pw)
+          home = pw->pw_dir;
+      }
+      if (!home || chdir(home) != 0) {
+        write(STDERR_FILENO, "ch: ", 4);
         write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        write(STDERR_FILENO, "\n", 1);
         return;
       }
-      i = 0;
-      while (home[i] != '\0' && i < 1023) {
-        new_dir[i] = home[i];
-        i++;
-      }
-      new_dir[i] = '\0';
-      /*(
-      if (chdir(home) != 0) {
-        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
-        return;
-      }
-      */
     }
-    // GO SOMEHWERE WITH ~/SOME/PATH
+
     else if (target[0] == '~') {
       const char *home = getenv("HOME");
-      if (home == NULL) {
+      if (!home) {
+        struct passwd *pw = getpwuid(getuid());
+        if (pw)
+          home = pw->pw_dir;
+      }
+      if (!home) {
+        write(STDERR_FILENO, "ch: ", 4);
         write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        write(STDERR_FILENO, "\n", 1);
         return;
       }
-      i = 0;
-      while (home[i] != '\0' && i < 1023) {
-        new_dir[i] = home[i];
-        i++;
+
+      char full_path[1024];
+      size_t home_len = strlen(home);
+      size_t extra_len = strlen(target + 1);
+      if (home_len + extra_len >= sizeof(full_path)) {
+        write(STDERR_FILENO, "ch: ", 4);
+        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        write(STDERR_FILENO, "\n", 1);
+        return;
       }
 
-      j = 1;
-      while (target[j] != '\0' && i < 1023) {
-        new_dir[i++] = target[j++];
+      memcpy(full_path, home, home_len);
+      memcpy(full_path + home_len, target + 1, extra_len + 1);
+
+      if (chdir(full_path) != 0) {
+        write(STDERR_FILENO, "ch: ", 4);
+        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        write(STDERR_FILENO, "\n", 1);
+        return;
       }
-      new_dir[i] = '\0';
     }
-    // GO TO PREVIOUS WITH -
+
     else if (strcmp(target, "-") == 0) {
       if (prev_dir[0] == '\0') {
-        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        const char *home = getenv("HOME");
+        if (!home) {
+          struct passwd *pw = getpwuid(getuid());
+          if (pw)
+            home = pw->pw_dir;
+        }
+        if (!home || chdir(home) != 0) {
+          write(STDERR_FILENO, "ch: ", 4);
+          write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+          write(STDERR_FILENO, "\n", 1);
+          return;
+        }
+      } else {
+        write(STDOUT_FILENO, prev_dir, strlen(prev_dir));
+        write(STDOUT_FILENO, "\n", 1);
+        char temp[1024];
+        strcpy(temp, cwd);
+
+        if (chdir(prev_dir) != 0) {
+          write(STDERR_FILENO, "ch: ", 4);
+          write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+          write(STDERR_FILENO, "\n", 1);
+          return;
+        }
+        strcpy(prev_dir, temp);
         return;
       }
-      write(STDERR_FILENO, prev_dir, strlen(prev_dir));
-      write(STDERR_FILENO, "\n", 1);
-
-      char temp[1024];
-      i = 0;
-      while (cwd[1] != '\0' && i < 1023) {
-        temp[i] = cwd[i];
-        i++;
-      }
-      temp[i] = '\0';
-      if (chdir(prev_dir) != 0) {
-        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
-        return;
-      }
-      i = 0;
-      while (temp[i] != '\0' && i < 1023) {
-        prev_dir[i] = temp[i];
-        i++;
-      }
-      prev_dir[i] = '\0';
-
-      return;
-    }
-
-    else {
-      i = 0;
-      while (target[i] != '\0' && i < 1023) {
-        new_dir[i] = target[i];
-        i++;
-      }
-      new_dir[i] = '\0';
-
-      if (chdir(new_dir) != 0) {
-        write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
-        return;
-      }
-
-      i = 0;
-      while (cwd[i] != '\0' && i < 1023) {
-        prev_dir[i] = cwd[i];
-        i++;
-      }
-      prev_dir[i] = '\0';
-      /*
+    } else {
       if (chdir(target) != 0) {
+        write(STDERR_FILENO, "ch: ", 4);
         write(STDERR_FILENO, CHDIR_ERROR_MSG, strlen(CHDIR_ERROR_MSG));
+        write(STDERR_FILENO, "\n", 1);
         return;
       }
-      */
     }
+    strcpy(prev_dir, cwd);
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     // HELP HANDLER
   } else if (strcmp(argv[0], "help") == 0) {
-    char *NEWL = "\n";
 
     if (argv[1] == NULL) {
-      char *HELP_EXIT = "exit: ";
-      write(STDOUT_FILENO, HELP_EXIT, strlen(HELP_EXIT));
-      write(STDOUT_FILENO, EXIT_HELP_MSG, strlen(EXIT_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
-      char *HELP_CD = "cd: ";
-      write(STDOUT_FILENO, HELP_CD, strlen(HELP_CD));
-      write(STDOUT_FILENO, CD_HELP_MSG, strlen(CD_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
-      char *HELP_PWD = "pwd: ";
-      write(STDOUT_FILENO, HELP_PWD, strlen(HELP_PWD));
-      write(STDOUT_FILENO, PWD_HELP_MSG, strlen(PWD_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
-      char *HELP_HELP = "help: ";
-      write(STDOUT_FILENO, HELP_HELP, strlen(HELP_HELP));
+      write(STDOUT_FILENO, "help: ", 6);
       write(STDOUT_FILENO, HELP_HELP_MSG, strlen(HELP_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
+      write(STDOUT_FILENO, "cd: ", 4);
+      write(STDOUT_FILENO, CD_HELP_MSG, strlen(CD_HELP_MSG));
+      write(STDOUT_FILENO, "\n", 1);
+      write(STDOUT_FILENO, "exit: ", 6);
+      write(STDOUT_FILENO, EXIT_HELP_MSG, strlen(EXIT_HELP_MSG));
+      write(STDOUT_FILENO, "\n", 1);
+      write(STDOUT_FILENO, "pwd: ", 5);
+      write(STDOUT_FILENO, PWD_HELP_MSG, strlen(PWD_HELP_MSG));
+      write(STDOUT_FILENO, "\n", 1);
+      write(STDOUT_FILENO, "history: ", 9);
+      write(STDOUT_FILENO, HISTORY_HELP_MSG, strlen(PWD_HELP_MSG));
+      write(STDOUT_FILENO, "\n", 1);
       return;
     }
 
@@ -187,7 +182,7 @@ void internal_handle(char **argv) {
       char *TMY = "cd: ";
       write(STDOUT_FILENO, TMY, strlen(TMY));
       write(STDOUT_FILENO, TMA_MSG, strlen(TMA_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
       return;
     }
 
@@ -195,28 +190,31 @@ void internal_handle(char **argv) {
       char *HELP_EXIT = "exit: ";
       write(STDOUT_FILENO, HELP_EXIT, strlen(HELP_EXIT));
       write(STDOUT_FILENO, EXIT_HELP_MSG, strlen(EXIT_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
+
     } else if (strcmp(argv[1], "cd") == 0) {
       char *HELP_CD = "cd: ";
       write(STDOUT_FILENO, HELP_CD, strlen(HELP_CD));
       write(STDOUT_FILENO, CD_HELP_MSG, strlen(CD_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
+
     } else if (strcmp(argv[1], "pwd") == 0) {
       char *HELP_PWD = "pwd: ";
       write(STDOUT_FILENO, HELP_PWD, strlen(HELP_PWD));
       write(STDOUT_FILENO, PWD_HELP_MSG, strlen(PWD_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
+
     } else if (strcmp(argv[1], "help") == 0) {
       char *HELP_HELP = "help: ";
       write(STDOUT_FILENO, HELP_HELP, strlen(HELP_HELP));
       write(STDOUT_FILENO, HELP_HELP_MSG, strlen(HELP_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
     } else {
       write(STDOUT_FILENO, argv[1], strlen(argv[1]));
       char *EXT = ": ";
       write(STDOUT_FILENO, EXT, strlen(EXT));
       write(STDOUT_FILENO, EXTERN_HELP_MSG, strlen(EXTERN_HELP_MSG));
-      write(STDOUT_FILENO, NEWL, strlen(NEWL));
+      write(STDOUT_FILENO, "\n", 1);
     }
   }
 }
@@ -225,16 +223,25 @@ void external_handle(char **argv, int bg) {
   pid_t pid = fork();
 
   if (pid == -1) {
+    write(STDERR_FILENO, "fork: ", 6);
     write(STDERR_FILENO, FORK_ERROR_MSG, strlen(FORK_ERROR_MSG));
+    write(STDERR_FILENO, "\n", 1);
     return;
   }
 
   if (pid == 0) {
-    execvpe(argv[0], argv, environ);
-    //
-  } else {
-    if (!bg) {
-      waitpid(pid, NULL, 0);
+    if (execvpe(argv[0], argv, environ) == -1) {
+      write(STDERR_FILENO, "exec: ", 6);
+      write(STDERR_FILENO, EXEC_ERROR_MSG, strlen(EXEC_ERROR_MSG));
+      write(STDERR_FILENO, "\n", 1);
+    } else {
+      if (!bg) {
+        if (waitpid(pid, NULL, 0)) {
+          write(STDERR_FILENO, "wait: ", 6);
+          write(STDERR_FILENO, WAIT_ERROR_MSG, strlen(EXEC_ERROR_MSG));
+          write(STDERR_FILENO, "\n", 1);
+        }
+      }
     }
   }
 }
